db4a3d98b3fa51554157aeae0e3f32e1e187c279
Philipp Messerschmidt <git@38911.net>
2018-12-03 16:08:27 +0100

    layout

--- PITCHME.md
+++ PITCHME.md
@@ -85,8 +85,21 @@ Philipp Messerschmidt - cs16-1/WS2018
 
 @[33-34, 46-59](data type linked list uses pointer-to-pointers)
 
++++?color=lavender
+@title[Struct addrinfo]
 
-
+```C
+struct addrinfo {
+    int              ai_flags;     // AI_PASSIVE, AI_CANONNAME, etc.
+    int              ai_family;    // AF_INET, AF_INET6, AF_UNSPEC
+    int              ai_socktype;  // SOCK_STREAM, SOCK_DGRAM
+    int              ai_protocol;  // use 0 for "any"
+    size_t           ai_addrlen;   // size of ai_addr in bytes
+    struct sockaddr *ai_addr;      // struct sockaddr_in or _in6
+    char            *ai_canonname; // full canonical hostname
+    struct addrinfo *ai_next;      // linked list, next node
+};
+```
 
 
 
--- template/src/c/main-improved.c
+++ template/src/c/main-improved.c
@@ -13,6 +13,20 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+
+// for reference
+/*struct addrinfo {
+    int              ai_flags;     // AI_PASSIVE, AI_CANONNAME, etc.
+    int              ai_family;    // AF_INET, AF_INET6, AF_UNSPEC
+    int              ai_socktype;  // SOCK_STREAM, SOCK_DGRAM
+    int              ai_protocol;  // use 0 for "any"
+    size_t           ai_addrlen;   // size of ai_addr in bytes
+    struct sockaddr *ai_addr;      // struct sockaddr_in or _in6
+    char            *ai_canonname; // full canonical hostname
+    struct addrinfo *ai_next;      // linked list, next node
+};*/
+
+
 #define PORT "80"  // the port users will be connecting to
 #define BACKLOG 10     // how many pending connections queue will hold
 
